ff975d32e393c5a4485264de8773a212cace970e
nomanwaseem111 <nomanwaseem111@gmail.com>
2023-01-22 19:25:37 +0500

    changes

--- src/components/Slides/index.jsx
+++ src/components/Slides/index.jsx
@@ -1,34 +1,52 @@
 import React from 'react'
 import 'react-slideshow-image/dist/styles.css'
 import { Slide } from 'react-slideshow-image';
+import { Typography, Button, Box, Stack } from "@mui/material";
+
 
 const index = () => {
   
     const fadeImages = [
         {
-          url: 'https://images.unsplash.com/photo-1671725779305-bff966b40cbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-          
+          link1: 'https://clean360.dexignlab.com/xhtml/images/client-logo/logo1.jpg',
+          link2:'https://clean360.dexignlab.com/xhtml/images/client-logo/logo1.jpg',
+          link3:'https://clean360.dexignlab.com/xhtml/images/client-logo/logo1.jpg',
+          link4:'https://clean360.dexignlab.com/xhtml/images/client-logo/logo1.jpg'
+
+
         },
         {
-          url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
-         
+          link1: 'https://clean360.dexignlab.com/xhtml/images/client-logo/logo1.jpg',
+          link2:'https://clean360.dexignlab.com/xhtml/images/client-logo/logo1.jpg',
+          link3:'https://clean360.dexignlab.com/xhtml/images/client-logo/logo1.jpg',
+          link4:'https://clean360.dexignlab.com/xhtml/images/client-logo/logo1.jpg'
+
         },
         {
-          url: 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
-         
+          link1: 'https://clean360.dexignlab.com/xhtml/images/client-logo/logo1.jpg',
+          link2:'https://clean360.dexignlab.com/xhtml/images/client-logo/logo1.jpg',
+          link3:'https://clean360.dexignlab.com/xhtml/images/client-logo/logo1.jpg',
+          link4:'https://clean360.dexignlab.com/xhtml/images/client-logo/logo1.jpg'
+
         },
       ];
   
     return (
-        <div className="slide-container" >
+      <Stack display={{md:'flex',xs:"flex"}} justifyContent={{md:"center",xs:'center'}} alignItems={{md:"center",xs:"center"}}>
+        <Stack  sx={{width:{md:"50%",xs:"100%"},height:{md:"auto",xs:'auto'},background:{md:"#fff",xs:'#fff'}}} >
         <Slide >
           {fadeImages.map((fadeImage, index) => (
-            <div key={index} >
-              <img style={{ width: '100%' }} src={fadeImage.url} />
-            </div>
+            <Stack display={{md:"flex",xs:"flex"}}  justifyContent={{md:"space-around",xs:"space-around"}} alignItems={{md:"center",xs:'center'}} flexDirection={{md:"row",xs:"row"}} key={index} >
+              <Box component="img" sx={{ width:{md:"20%",xs:"20%"},height:{md:"100px",xs:'100px'} }} src={fadeImage.link1} />
+              <Box component="img" sx={{ width:{md:"20%",xs:"20%"},height:{md:"100px",xs:'100px'} }} src={fadeImage.link2} />
+              <Box component="img" sx={{ width:{md:"20%",xs:"20%"},height:{md:"100px",xs:'100px'} }} src={fadeImage.link3} />
+              <Box component="img" sx={{ width:{md:"20%",xs:"20%"},height:{md:"100px",xs:'100px'} }} src={fadeImage.link4} />
+
+            </Stack>
           ))}
         </Slide>
-      </div>
+      </Stack>
+      </Stack>
   )
 }
 
