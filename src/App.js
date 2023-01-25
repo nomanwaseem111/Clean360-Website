8ea8025a4eef47894594a7c92ef66b4387af0af1
rafaekhatri11 <rafaekhatri11@gmail.com>
2023-01-25 22:05:09 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Footer from "./components/Footer";
-import './index.css'
+import React, { createContext, useContext } from "react"
+import Navbar from "./components/Navbar"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./components/Home"
+import Footer from "./components/Footer"
+import "./index.css"
+import { AppContext } from "./Context"
+
+
+export const data = createContext()
+
 
 const App = () => {
+//  const context = useContext(AppContext);
+
+   
+ const name = "Noman"
+
+
+ const homeRef = React.useRef(null)
+  const whyUsRef = React.useRef(null)
+  const servicesRef = React.useRef(null)
+  const aboutRef = React.useRef(null)
+  const contactRef = React.useRef(null)
+
+  const scrollToSection = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: "smooth",
+    })
+  } 
+
+
+  
+
   return (
-    <>
+    <data.Provider value={{homeRef,whyUsRef,servicesRef,aboutRef,contactRef,scrollToSection}}>
+    
       <Router>
         <div>
           <Navbar />
-
-          <Routes>
-            <Route path="/" element={<Home />} />
-
-            {/* <Route path="/whyus" element={<WhyUs />} />
-            <Route path="/services" element={<Services />} />
-
-            <Route path="/about" element={<About />} />
-
-            <Route path="/contact" element={<Contact />} /> */}
-
-          </Routes>
+          <Home />
 
           <Footer />
         </div>
       </Router>
-    </>
-  );
-};
+    </data.Provider>
+  )
+}
 
-export default App;
+export default App
